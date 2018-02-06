d750ea7303791fa6c77ecf1a1f49b5a7a02037c9
Jiri Lahoda <jlahoda@deloittece.com>
2018-02-06 16:14:30 +0100

    Aura test.

--- src/aura/TestingComponent/TestingComponentController.js
+++ src/aura/TestingComponent/TestingComponentController.js
@@ -1,6 +1,7 @@
 ({
     toggle: function (cmp, event) {
         var spinner = cmp.find("mySpinner");
+        var spinner2 = cmp.find("mySpinner");
         $A.util.toggleClass(spinner, "slds-hide");
     }
 })
